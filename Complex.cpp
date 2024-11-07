49fbdd2443a8e10558834722bdd3e26dd7ed26d3
Sunho Kim <107231365+sunhome243@users.noreply.github.com>
2024-11-07 14:29:43 -0500

    added functionalities

--- Complex.cpp
+++ Complex.cpp
@@ -15,6 +15,7 @@ using namespace std;
 //========================================================
 // default constructor
 // Developer: Sunho Kim
+// This constructor initializes a complex number with real and imaginary parts set to 0
 //========================================================
 Complex::Complex()
 {
@@ -24,6 +25,7 @@ Complex::Complex()
 //========================================================
 // copy constructor
 // Developer: Sunho Kim
+// This constructor creates a new complex number as a copy of an existing one
 //========================================================
 Complex::Complex(const Complex &c)
 {
@@ -33,6 +35,7 @@ Complex::Complex(const Complex &c)
 //========================================================
 // Complex(a,b) constructor
 // Developer: Sunho Kim
+// This constructor creates a complex number with specified real and imaginary parts
 //========================================================
 Complex::Complex(double x, double y)
 {
@@ -66,6 +69,7 @@ Complex::Complex(double x, double y)
 //========================================================
 // addition operators
 // Developer: Sunho Kim
+// These operators handle addition of complex numbers with other complex numbers, doubles, and integers
 //========================================================
 Complex Complex::operator+(const Complex &c) const
 {
@@ -96,6 +100,7 @@ Complex Complex::operator+(int i) const
 //========================================================
 // subtraction operators
 // Developer: Sunho Kim
+// These operators handle subtraction of complex numbers with other complex numbers, doubles, and integers
 //========================================================
 Complex Complex::operator-(const Complex &c) const
 {
@@ -125,12 +130,68 @@ Complex Complex::operator-(int i) const
 }
 //========================================================
 // multiplication operators
+// developer: Sunho Kim
+// These operators handle multiplication of complex numbers with other complex numbers, doubles, and integers
 //========================================================
+Complex Complex::operator*(const Complex &c) const
+{
+    Complex comp;
 
+    comp.a = a * c.a - b * c.b;
+    comp.b = a * c.b + b * c.a;
+
+    return comp;
+}
+Complex Complex::operator*(double f) const
+{
+    Complex comp;
+
+    comp.a = a * f;
+    comp.b = b*f;
+
+    return comp;
+}
+Complex Complex ::operator*(int i) const
+{
+    Complex comp;
+
+    comp.a = a * i;
+    comp.b = b * i;
+
+    return comp;
+}
 //========================================================
 // division operators
+// Developer: Sunho Kim
+// These operators handle division of complex numbers with other complex numbers, doubles, and integers
 //========================================================
+Complex Complex::operator/(const Complex &c) const
+{
+    Complex comp;
 
+    double denominator = c.a * c.a + c.b * c.b;
+    comp.a = (a * c.a + b * c.b) / denominator;
+    comp.b = (b * c.a - a * c.b) / denominator;
+
+    return comp;
+}
+Complex Complex::operator/(double f) const
+{
+    Complex comp;
+    comp.a = a / f;
+    comp.b = b / f;
+
+    return comp;
+}
+Complex Complex ::operator/(int i) const
+{
+    Complex comp;
+
+    comp.a = a / i;
+    comp.b = b / i;
+
+    return comp;
+}
 //========================================================
 // exponentiation
 //========================================================
@@ -141,23 +202,44 @@ Complex Complex::operator-(int i) const
 
 //========================================================
 // abs
+// Developer: Sunho Kim
+// This method calculates the absolute value (magnitude) of the complex number
 //========================================================
-
+double Complex::abs() const
+{
+    return sqrt(a * a + b * b);
+}
 //========================================================
 // equality operator
+// Developer: Sunho Kim
+// This operator checks if two complex numbers are equal
 //========================================================
-
+bool Complex::operator==(const Complex &c) const
+{
+    return (a == c.a && b == c.b);
+}
 //========================================================
 // inequality operator
+// Developer: Sunho Kim
+// This operator checks if two complex numbers are not equal
 //========================================================
-
+bool Complex::operator!=(const Complex &c) const {
+    return (a != c.a || b != c.b);
+}
 //========================================================
 // overload >> for cin
+// Developer: Sunho Kim
+// This operator handles the input of complex numbers
 //========================================================
-
+istream &operator>>(istream &is, Complex &c)
+{
+    is >> c.a >> c.b;
+    return is;
+}
 //========================================================
 // overload << for cout
 // Developer: Sunho Kim
+// This operator handles the output formatting of complex numbers
 //========================================================
 ostream &operator<<(ostream &os, const Complex &c)
 {
@@ -171,6 +253,14 @@ ostream &operator<<(ostream &os, const Complex &c)
     }
     else
     {
-        os << c.a << c.b << "i";
+        if (c.b < 0)
+        {
+            os << c.a << c.b << "i";
+        }
+        else
+        {
+            os << c.a << "+" << c.b << "i";
+        }
     }
+    return os;
 }
